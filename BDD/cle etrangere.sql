5c9b452d544c9e3cb6fc946e1ca26e54968e0f3c
etihaine <78075407+etihaine@users.noreply.github.com>
2022-09-19 17:31:59 +0200

    dossier
    création des premiers fichiers et des dossier

--- BDD/cle etrangere.sql
+++
@@ -1,5 +0,0 @@
-alter table Attribution
-  add constraint fk1_Attribution
-   foreign key(idEtab) references Etablissement(id),
-  add constraint fk2_Attribution
-   foreign key(idGroupe) references Groupe(id);
