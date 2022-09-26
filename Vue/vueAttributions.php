e78a9a953d4eec675267399f384a81c0f77e058a
etihaine <78075407+etihaine@users.noreply.github.com>
2022-09-26 16:07:07 +0200

    début attribution

---
+++ Vue/vueAttributions.php
@@ -0,0 +1,8 @@
+<?php $title = 'Festival - Attribution'; ?>
+
+
+<?php ob_start() ?>
+<p>Une erreur est survenue : <?= $msgErreur ?></p>
+<?php $contenu = ob_get_clean(); ?>
+
+<?php require 'pageTemplate.php'; ?>
