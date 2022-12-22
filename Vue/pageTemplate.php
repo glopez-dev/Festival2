2fd5135eeca28908078f392454ef348e383b20f5
glopez <gabriellopezfouche@gmail.com>
2022-12-22 16:19:28 +0100

    MVC POO 100% fonctionnel

--- Vue/pageTemplate.php
+++ Vue/pageTemplate.php
@@ -1,32 +1,33 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
-"http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <!-- TITRE ET MENUS -->
 <html lang="fr">
+
 <head>
-<title><?php echo $title ?></title>
-<meta http-equiv="Content-Language" content="fr">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link href="Contenu/cssSauvegarde.css" rel="stylesheet" type="text/css">
+   <title><?php echo $title ?></title>
+   <meta http-equiv="Content-Language" content="fr">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+   <link href="assets/style.css" rel="stylesheet" type="text/css">
 </head>
+
 <body class="basePage">
 
-<!--  Tableau contenant le titre -->
-<table width="100%" cellpadding="0" cellspacing="0">
-   <tr> 
-      <td class="titre">Festival Sp'Or <br>
-      <span id="texteNiveau2" class="texteNiveau2">
-      H&eacute;bergement des participants</span><br>&nbsp;
-      </td>
-   </tr>
-</table>
-<!--  Tableau contenant les menus -->
-<table width="80%" cellpadding="0" cellspacing="0" class="tabMenu" align="center">
-   <tr>
-      <td class="menu"><a href="index.php">Accueil</a></td>
-      <td class="menu"><a href="index.php?action=listeEtablissements">
-      Gestion établissements</a></td>
-      <td class="menu"><a href="index.php?action=consultationAttributions">
-      Attributions chambres</a></td>
-   </tr>
-</table>
-<br>
+   <!--  Tableau contenant le titre -->
+   <table width="100%" cellpadding="0" cellspacing="0">
+      <tr>
+         <td class="titre">Festival Sp'Or <br>
+            <span id="texteNiveau2" class="texteNiveau2">
+               H&eacute;bergement des participants</span><br>&nbsp;
+         </td>
+      </tr>
+   </table>
+   <!--  Tableau contenant les menus -->
+   <table width="80%" cellpadding="0" cellspacing="0" class="tabMenu" align="center">
+      <tr>
+         <td class="menu"><a href="index.php">Accueil</a></td>
+         <td class="menu"><a href="index.php?action=listeEtablissements">
+               Gestion établissements</a></td>
+         <td class="menu"><a href="index.php?action=consultationAttributions">
+               Attributions chambres</a></td>
+      </tr>
+   </table>
+   <br>
